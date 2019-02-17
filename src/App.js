3f0ef4e7c307843a9a67bda1f605691bfb8a96ac
Brandon Jurado <brandon.jurado@aa.com>
2019-02-17 03:59:41 -0600

    Display 3d object

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import ObjectScene from './components/ObjectScene';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ObjectScene />
+      </div>
+    );
+  }
+}
+
+export default App;
